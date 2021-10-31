b57ee15cd17a4030debc68e58752db40479f57ce
TsuMakoto <tsukamoto@fusic.co.jp>
2021-10-31 15:34:05 +0900

    Add rails5 - example

---
+++ example/rails5/db/migrate/20211031043641_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :age
+      t.string :role
+
+      t.timestamps
+    end
+  end
+end
---
+++ example/rails5/test/controllers/people_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class PeopleControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @person = people(:one)
+  end
+
+  test "should get index" do
+    get people_url, as: :json
+    assert_response :success
+  end
+
+  test "should create person" do
+    assert_difference('Person.count') do
+      post people_url, params: {
+        people: {
+          name: "test01",
+          age: 10,
+          role: 0
+        }
+      }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show person" do
+    get person_url(@person), as: :json
+    assert_response :success
+  end
+
+  test "should update person" do
+    patch person_url(@person), params: {
+      people: {
+        name: "test01",
+        age: 10,
+        role: 0
+      }
+    }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy person" do
+    assert_difference('Person.count', -1) do
+      delete person_url(@person), as: :json
+    end
+
+    assert_response 204
+  end
+end
