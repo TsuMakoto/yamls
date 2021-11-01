df19ec18a3fc948625095f3c58d423353de0bcc3
TsuMakoto <tsukamoto@fusic.co.jp>
2021-11-01 11:40:17 +0900

    Add define-method - {controller_name}_{action_name}_params

---
+++ example/rails-demo/db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_01_020803) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "label"
+    t.integer "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.integer "role"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- lib/yamls/support/parameters.rb
+++ lib/yamls/support/parameters.rb
@@ -7,12 +7,24 @@ module Yamls
     module Parameters
       extend ActiveSupport::Concern
 
+      included do
+        before_action :define_params_method
+      end
+
+      protected
+
       def yamls
         Yamls::Parameters.new(
           params,
           model: controller_name.singularize,
           action: action_name,
         ).permit
+      end
+
+      def define_params_method
+        method_name = "#{controller_name.singularize}_#{action_name}_params"
+
+        define_singleton_method(method_name) { yamls }
       end
     end
   end
